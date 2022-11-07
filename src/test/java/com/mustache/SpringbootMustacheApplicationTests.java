151e7da2fb2163dba317446abf68eaf911b18f6b
Kim-Ji-Yeong <yeong6415@daum.net>
2022-11-07 11:47:42 +0900

    Initial commit

---
+++ src/test/java/com/mustache/SpringbootMustacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
