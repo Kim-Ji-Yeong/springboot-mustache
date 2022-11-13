fac0e0be3a5ef0e2ec4c3ab6aecfd67344857d7e
Kim-Ji-Yeong <yeong6415@daum.net>
2022-11-13 22:24:18 +0900

    수정중

--- src/test/java/com/mustache/SpringbootMustacheApplicationTests.java
+++ src/test/java/com/mustache/SpringbootMustacheApplicationTests.java
@@ -1,3 +1,12 @@
 package com.mustache;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
+class SpringbootMustacheBbs2ApplicationTests {
 
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
